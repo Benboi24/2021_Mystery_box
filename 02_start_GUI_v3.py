adf7a9803078e0c275cb9ab9956de6a288a1e675
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-03-22 12:20:28 +1300

    2 components
    mostly working code

--- 02_start_GUI_v3.py
+++ 02_start_GUI_v3.py
@@ -1,3 +1,5 @@
+# v3, stakes buttons only enabled if there are sufficient funds
+
 from tkinter import *
 from functools import partial  # To prevent unwanted windows
 import random
@@ -9,6 +11,10 @@ class Start:
         # GUI to get starting balance and stakes
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
+
+        # Set Initial balance to zero
+        self.starting_funds = IntVar()
+        self.starting_funds.set(0)
 
         # Mystery Heading (row 0)
         self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game",
@@ -25,10 +31,25 @@ class Start:
                                           wrap=275, justify=LEFT, padx=10, pady=10)
         self.mystery_instructions.grid(row=1)
 
-        # Entry box... (row 2)
-        self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
-        self.start_amount_entry.grid(row=2)
+        # Entry box, Button & Error label (row 2)
+        self.entry_error_frame = Entry(self.start_frame, width=200)
+        self.entry_error_frame.grid(row=2)
 
+        self.start_amount_entry = Entry(self.entry_error_frame,
+                                        font="Arial 19 bold", width=10)
+        self.start_amount_entry.grid(row=0, colum=0)
+
+        self.add_funds_button = Button(self.entry_error_frame,
+                                       font="Arial 14 bold",
+                                       text="Add Funds",
+                                       command=self.check_funds)
+        self.add_funds_button.grid(row=0, column=1)
+
+        self.amount_error_label = Label(self.entry_error_frame, fg="maroon",
+                                        text="", font="Arial 10 bold", wrap=275,
+                                        justify=LEFT)
+        self.amount_error_label.grid(row=1, columnspan=2, pady=5)
+        
         # button frame (row 3)
         self.stakes_frame = Frame(self.start_frame)
         self.stakes_frame.grid(row=3)
