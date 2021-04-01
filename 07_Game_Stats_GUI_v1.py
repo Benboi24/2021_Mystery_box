396fa29edf4d91ffe98d6c1200dc24643a773178
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-04-01 14:39:44 +1300

    07_Game_Stats_GUI_v1.py
    more code

--- 07_Game_Stats_GUI_v1.py
+++ 07_Game_Stats_GUI_v1.py
@@ -62,7 +62,68 @@ class GameStats:
 
         # To Export <instructions> (row 1)
         self.export_instructions = Label(self.stats_frame,
-                                         text="Here are your Game Statistics")
+                                         text="Here are your Game Statistics."
+                                              "Please use the Export button to "
+                                              "access the results of each "
+                                              "round that you played", wrap=250,
+                                         font="arial 10 italic",
+                                         justify=LEFT, fg="green",
+                                         padx=10, pady=10)
+        self.export_instructions.grid(row=1)
+
+        # Starting Balance (row 2)
+        self.details_frame = Frame(self.stats_frame)
+        self.details_frame.grid(row=2)
+
+        # Starting Balance (row 2.0)
+
+        self.stats_balance_label = Label(self.details_frame,
+                                         text="Starting Balance:", font=heading,
+                                         anchor="e")
+        self.stats_balance_label.grid(row=0, column=0, padx=0)
+
+        self.stats_balance_value_label = Label(self.details_frame, font=content,
+                                               text="${}".format(game_stats[0]),
+                                               anchor="w")
+        self.stats_balance_value_label.grid(row=0, column=1, padx=0)
+
+        # Current Balance (row 2.2)
+        self.current_balance_label = Label(self.details_frame,
+                                           text="Current Balance:", font=heading,
+                                           anchor="e")
+        self.current_balance_label.grid(row=1, column=0, padx=0)
+
+        self.current_balance_value_label = Label(self.details_frame, font=content,
+                                                 text="${}".format(game_stats[1]),
+                                                 anchor="w")
+        self.current_balance_value_label.grid(row=1, column=1, padx=0)
+
+        if game_stats[1] > game_stats[0]:
+            win_loss = "Amount Won:"
+            amount = game_stats[1] - game_stats[0]
+            win_loss_fg = "#green"
+        else:
+            win_loss = "Amount Lost:"
+            amount = game_stats[0] - game_stats[1]
+            win_loss_fg = "#660000"
+
+        # Amount won / lost (row 2,3)
+        self.wind_loss_label = Label(self.details_frame,
+                                     text=win_loss, font=heading,
+                                     anchor="e")
+        self.wind_loss_label.grid(row=2, column=0, padx=0)
+
+
+
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Mystery Box Game")
+    something = Start(root)
+    root.mainloop()
 
 
 
