9cdaf3d13fa4595db596107bdae30b93c3063f45
Benboi24 <60630518+Benboi24@users.noreply.github.com>
2021-04-01 14:24:11 +1300

    07_Game_Stats_v1.py
    more code.

--- 07_Game_Stats_GUI_v1.py
+++ 07_Game_Stats_GUI_v1.py
@@ -21,4 +21,73 @@ class Game:
                                    font="Arial 24 bold", padx=10,
                                    pady=10)
         self.heading_label.grid(row=0)
-        
+
+        # History Button (row 1)
+        self.stats_button = Button(self.game_frame,
+                                   text="Game Stats",
+                                   font="Arial 14", padx=10, pady=10,
+                                   command = lambda: self.to_stats(self.round_stats_list))
+        self.stats_button.grid(row=1)
+
+    def to_stats(self, game_history, game_stats):
+        GameStats(self, game_history, game_stats)
+
+
+class GameStats:
+    def __init__(self, partner, game_history, game_stats):
+
+        print(game_history)
+
+        # disable help button
+        partner.stats_button.config(state=DISABLED)
+
+        heading = "Arial 12 bold"
+        content = "Arial 12"
+
+        # Sets up child window (ie: help box)
+        self.stats_box = Toplevel()
+
+        # If users press cross at top, closes help and 'releases' help button
+        self.stats_box.protocol('WM_DELETE_WINDOW', partial(self.close_stats,
+                                                            partner))
+
+        # Set up GUI Frame
+        self.stats_frame = Frame(self.stats_box)
+        self.stats_frame.grid()
+
+        # Set up Help heading (row 0)
+        self.stats_heading_label = Label(self.stats_frame, text="Game Statistics",
+                                         font="arial 19 bold")
+        self.stats_heading_label.grid(row=0)
+
+        # To Export <instructions> (row 1)
+        self.export_instructions = Label(self.stats_frame,
+                                         text="Here are your Game Statistics")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
